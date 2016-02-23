a53da458df35406364974bf43ed9b08ec0860f20
karzler <mmishra23@gmail.com>
2016-02-23 07:52:53 +0530

    Multilevel menu added.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,32 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+  $scope.groups = [];
+  for (var i=0; i<2; i++) {
+      $scope.groups[i] = {
+        name: i,
+        items: []
+      };
+      for (var j=0; j<8; j++) {
+        $scope.groups[i].items.push( ' Sub-item -' + j);
+      }
+  }
+  
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+
   // Form data for the login modal
   $scope.loginData = {};
 
