77108447776ea69f9e625b963d1bac3f329c6a11
Pranjal Mittal <mittal.pranjal@gmail.com>
2016-02-28 22:29:35 -0800

    Stripped out code in starter to bare min. 2 menus - Home, Posts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,85 +28,9 @@ angular.module('mumblr.controllers', ['ionic','ngCordova'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
 
-  //$scope.snippet = "<h1>Rendering HTML within ionic. Yayyy!!</h1>";
 
-
-  $scope.posts_right = [];
-  for (var i=0; i < 10; i++) {
-    $scope.posts_right[i] = {
-      name: 'Post ' + i.toString()
-    };
-  }
-
-  $scope.groups = [];
-  for (var i=0; i<2; i++) {
-      $scope.groups[i] = {
-        name: i,
-        items: []
-      };
-      for (var j=0; j<8; j++) {
-        $scope.groups[i].items.push( ' Sub-item -' + j);
-      }
-  }
-  
-  /*
-   * if given group is the selected group, deselect it
-   * else, select the given group
-   */
-  $scope.toggleGroup = function(group) {
-    if ($scope.isGroupShown(group)) {
-      $scope.shownGroup = null;
-    } else {
-      $scope.shownGroup = group;
-    }
-  };
-
-  $scope.isGroupShown = function(group) {
-    return $scope.shownGroup === group;
-  };
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
-.controller('BookmarksCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
 
 .controller('HomeCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
 
